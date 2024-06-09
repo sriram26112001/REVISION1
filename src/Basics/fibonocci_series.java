1382d4bd5c37d0cd397762ca9000c8c13be8d553
Sriram <sriram26112001@gmail.com>
2024-06-09 15:12:41 +0530

    fibonocci series program has been implemented

--- src/Basics/fibonocci_series.java
+++ src/Basics/fibonocci_series.java
@@ -3,6 +3,35 @@ import java.util.Scanner;
 public class fibonocci_series {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-
+        System.out.println("enter the nth  fibonocci number which you want to get");
+        int n=in.nextInt();
+        int ans=fibonocci_finder(n);
+        System.out.println("the nth fibonocci number is"+ans);
+    }
+    public static int fibonocci_finder(int n)
+    {
+        int first=0;
+        int second=0;
+        if(n==0)
+        {
+            return first;
+        }
+        else if(n==1)
+        {
+            return second;
+        }
+        else
+        {
+            int t=n-2;
+            int total;
+            while(t!=0)
+            {
+                total=first+second;
+                first=second;
+                second=total;
+                t--;
+            }
+            return second;
+        }
     }
 }
