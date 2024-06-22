1c2a335e61421c1dd18df47afbbf1011d5e4cf4f
Sriram <sriram26112001@gmail.com>
2024-06-22 16:45:48 +0530

    Kadanes algorithm Implemented

---
+++ src/Arrays/Maximum_Subarray_sum.java
@@ -0,0 +1,35 @@
+package Arrays;
+import java.util.*;
+public class Maximum_Subarray_sum {
+    public static void main(String[] args) {
+        int i;
+        int[] arr=new int[8];
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the elements of the array");
+        for(i=0;i<arr.length;i++)
+        {
+            arr[i]=in.nextInt();
+        }
+        int max_sum=maximum_sum_finder(arr);
+        System.out.println(max_sum);
+    }
+    public static int maximum_sum_finder(int[] arr)
+    {
+        int sum=0;
+        int i;
+        int ans=Integer.MIN_VALUE;
+        for(i=0;i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+            if(sum>ans)
+            {
+                ans=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return ans;
+    }
+}
